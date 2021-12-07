5e7c3a998674dc604b3295858cd10dbc35228671
Trenton Sauer <trentonsauer@gmail.com>
2021-12-06 20:03:21 -0600

    Init commit

---
+++ src/main/java/com/safnaliftoff/upscaile/UpscaileApplication.java
@@ -0,0 +1,13 @@
+package com.safnaliftoff.upscaile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpscaileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpscaileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safnaliftoff/upscaile/UpscaileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safnaliftoff.upscaile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpscaileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
