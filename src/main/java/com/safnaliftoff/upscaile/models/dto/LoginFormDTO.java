54c13e5b2d1d59666863035717b32aa04895106a
DarrenFritts <84247704+DarrenFritts@users.noreply.github.com>
2021-12-26 20:00:12 -0600

    cleanup with no updates

--- src/main/java/com/safnaliftoff/upscaile/models/dto/LoginFormDTO.java
+++ src/main/java/com/safnaliftoff/upscaile/models/dto/LoginFormDTO.java
@@ -17,18 +17,22 @@ public class LoginFormDTO {
     private String password;
 
     public String getUsername() {
+
         return username;
     }
 
     public void setUsername(String username) {
+
         this.username = username;
     }
 
     public String getPassword() {
+
         return password;
     }
 
     public void setPassword(String password) {
+
         this.password = password;
     }
 
