715e26e79cc9a575580668fb9bbefd968fe52c70
Trenton Sauer <trentonsauer@gmail.com>
2022-01-06 16:11:43 -0600

    Images are associated with user ids.

--- src/main/java/com/safnaliftoff/upscaile/controllers/HomeController.java
+++ src/main/java/com/safnaliftoff/upscaile/controllers/HomeController.java
@@ -1,7 +1,9 @@
 package com.safnaliftoff.upscaile.controllers;
 
 import com.safnaliftoff.upscaile.data.ImageRepository;
+import com.safnaliftoff.upscaile.data.UserRepository;
 import com.safnaliftoff.upscaile.models.Image;
+import com.safnaliftoff.upscaile.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -22,12 +25,18 @@ public class HomeController {
 
     @Autowired
     private ImageRepository imageRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private AuthenticationController authenticationController;
 
     @GetMapping
     public String main() { return "index"; }
 
     @PostMapping
-    public String uploadImage(@RequestParam("file") MultipartFile file) throws Exception {
+    public String uploadImage(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws Exception {
+
+        User currentUser=  authenticationController.getUserFromSession(request.getSession());
 
         try {
             Runtime runTime = Runtime.getRuntime();
@@ -36,8 +45,8 @@ public class HomeController {
             Files.write(input_path, file.getBytes());
 
             Path output_path = Paths.get(HomeController.class.getResource("/").getPath().substring(1) + new Date().getTime() + "-" + file.getOriginalFilename());
-            System.out.println(input_path.toString());
-            System.out.println(output_path.toAbsolutePath().toString().replace("\\", "\\\\"));
+//            System.out.println(input_path.toString());
+//            System.out.println(output_path.toAbsolutePath().toString().replace("\\", "\\\\"));
             String[] params = new String[] {
                     "realesrgan-ncnn-vulkan-v0.1.3.2\\windows\\realesrgan-ncnn-vulkan-v0.1.3.2-windows\\realesrgan-ncnn-vulkan.exe",
                     "-i", "realesrgan-ncnn-vulkan-v0.1.3.2\\img_in\\" + file.getOriginalFilename(),
@@ -48,7 +57,7 @@ public class HomeController {
             Process process = pb.start();
             process.waitFor();
 
-            imageRepository.save(new Image(file.getOriginalFilename(), output_path.toAbsolutePath().toString()));
+            imageRepository.save(new Image(file.getOriginalFilename(), output_path.toAbsolutePath().toString(), currentUser));
 
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/com/safnaliftoff/upscaile/models/User.java
+++ src/main/java/com/safnaliftoff/upscaile/models/User.java
@@ -3,7 +3,11 @@ package com.safnaliftoff.upscaile.models;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class User extends AbstractEntity {
@@ -14,6 +18,10 @@ public class User extends AbstractEntity {
     @NotNull
     private String pwHash;
 
+    @OneToMany()
+    @JoinColumn(name= "user_id")
+    private List<Image> images = new ArrayList<>();
+
     private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     public User() {}
@@ -21,6 +29,12 @@ public class User extends AbstractEntity {
     public User(String username, String password) {
         this.username = username;
         this.pwHash = encoder.encode(password);
+    }
+
+    public List<Image> getImages() { return images; }
+
+    public void setImages(List<Image> images) {
+        this.images = images;
     }
 
     public String getUsername() {
