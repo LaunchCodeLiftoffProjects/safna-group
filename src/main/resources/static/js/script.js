f942fc986895cb9e660b8ae10e2c068149f00585
Trenton Sauer <trentonsauer@gmail.com>
2022-01-17 01:50:40 -0600

    fixed thumbnail display; comparison slider broken in image modal

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -208,3 +208,32 @@ function initComparisons() {
     }
   }
 }
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var images = $('.img-thumbnail');
+
+for (var i=0; i<images.length; i++) {
+    var image = images[i];
+    image.onclick = openModalImage;
+}
+
+function openModalImage() {
+    modal.style.display = "block";
+    var modalImgLo = document.getElementById("img_lo");
+    var modalImgHi = document.getElementById("img_hi");
+    modalImgLo.src = this.alt;
+    modalImgHi.src = this.src;
+}
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
